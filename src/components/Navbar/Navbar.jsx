77292e356f4b42fdc7b9598a3f4e29de3124e22a
Jadesadaphon chaykaew <jadesadaphon.chaykaew@gmail.com>
2025-05-08 01:22:44 +0700

    update Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -150,7 +150,7 @@ const Navbar = () => {
             ))}
             <div className="flex space-x-4">
               <a
-                href="https://github.com/codingmastr"
+                href="https://github.com/jadesadaphon"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-300 hover:text-white"
